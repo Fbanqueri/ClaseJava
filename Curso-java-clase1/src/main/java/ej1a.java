3691575ec37fddae5c1e33d2952709e0323fdee3
Fbanqueri <Federicobanqueri@hotmail.com>
2023-03-01 21:46:08 -0300

    clase1

---
+++ Curso-java-clase1/src/main/java/ej1a.java
@@ -0,0 +1,17 @@
+
+public class ej1a {
+
+	public static void main(String[] args) {
+
+		int numeroInicio = 5;
+		int numeroFin = 14;
+
+		while (numeroInicio <= numeroFin) {
+			System.out.println(numeroInicio);
+			numeroInicio++;
+		}
+		
+
+	}
+
+}
---
+++ Curso-java-clase1/src/main/java/ej1b.java
@@ -0,0 +1,18 @@
+
+public class ej1b {
+
+	public static void main(String[] args) {
+
+		int numeroInicio = 5;
+		int numeroFin = 14;
+
+		while (numeroInicio <= numeroFin) {
+			if (numeroInicio % 2 == 0) {
+				System.out.println(numeroInicio);
+			}
+			numeroInicio++;
+		}
+
+	}
+
+}
